130d22368462262ed324237b7a0275c2dbb92d66
Feyyaz ONUR <feyyazonur0@gmail.com>
2024-06-24 00:00:30 +0300

    first commit

---
+++ Things To Do/Things_To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Things_To_DoApp.swift
+//  Things To Do
+//
+//  Created by Feyyaz ONUR on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Things_To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
