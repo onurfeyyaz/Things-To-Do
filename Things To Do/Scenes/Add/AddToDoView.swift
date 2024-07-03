3450a6990b63e4a9d155d988839be85b9329edf7
Feyyaz ONUR <feyyazonur0@gmail.com>
2024-07-04 02:52:35 +0300

    add tabbar

---
+++ Things To Do/Scenes/Add/AddToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  AddToDoView.swift
+//  Things To Do
+//
+//  Created by Feyyaz ONUR on 4.07.2024.
+//
+
+import SwiftUI
+
+struct AddToDoView: View {
+    var body: some View {
+        Text("Adding screen")
+    }
+}
+
+#Preview {
+    AddToDoView()
+}
---
+++ Things To Do/Scenes/Add/AddToDoViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  AddToDoViewModel.swift
+//  Things To Do
+//
+//  Created by Feyyaz ONUR on 4.07.2024.
+//
+
+import Foundation
+
+protocol AddToDoViewModelProtocol {
+    
+}
+
+final class AddToDoViewModel {
+    
+}
+
+extension AddToDoViewModel: AddToDoViewModelProtocol {
+    
+}
