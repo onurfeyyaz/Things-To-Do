4afef509702673eafe927efb0ca66a53cf7da1e2
Feyyaz ONUR <feyyazonur0@gmail.com>
2024-07-20 01:48:09 +0300

    update to do item model

--- Things To Do/Models/ToDoItem.swift
+++ Things To Do/Models/ToDoItem.swift
@@ -7,8 +7,66 @@
 
 import Foundation
 
+enum ToDoStatus: Codable {
+    case todo
+    case doing
+    case done
+}
+
+enum ToDoPriority: Codable {
+    case low
+    case medium
+    case high
+}
+
 struct ToDoItem: Identifiable, Codable {
-    var id = UUID()
+    let id: UUID
     var title: String
+    var description: String
+    var category: String
+    var location: String?
     var isCompleted: Bool
+    var creationDate: Date
+    var deadline: Date?
+    var reminder: Date?
+    var status: ToDoStatus
+    var priority: ToDoPriority
+    var tags: [String]
+    var attachments: [URL]
+    var completionPercentage: Double {
+        switch status {
+        case .todo:
+            0.0
+        case .doing:
+            50.0
+        case .done:
+            100.0
+        }
+    }
+    
+    init(title: String, 
+         description: String,
+         category: String = "",
+         location: String? = nil,
+         deadline: Date? = nil,
+         reminder: Date? = nil,
+         priority: ToDoPriority = .low,
+         status: ToDoStatus = .todo,
+         tags: [String] = [],
+         attachments: [URL] = []) {
+        
+        self.id = UUID()
+        self.title = title
+        self.description = description
+        self.category = category
+        self.location = location
+        self.isCompleted = false
+        self.creationDate = Date()
+        self.deadline = deadline
+        self.reminder = reminder
+        self.priority = priority
+        self.status = status
+        self.tags = tags
+        self.attachments = attachments
+    }
 }
